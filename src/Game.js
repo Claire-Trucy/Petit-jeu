e8dfb776b7b6b070777ddd5eb147e5aa96e33613
Claire-Trucy <trucy.claire92@gmail.com>
2023-03-19 09:47:18 +0100

    rules of the game changed

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './game.css';
 
 export default function Game({ username }) {
   const [position, setPosition] = useState({ x: 1, y: 1 });
@@ -23,7 +24,7 @@ export default function Game({ username }) {
         default:
           break;
       }
-      if (newPosition.x === 8 && newPosition.y === 8) {
+      if (newPosition.x === 9 && newPosition.y === 9) {
         setGameOver(true);
       } else {
         setPosition(newPosition);
@@ -37,18 +38,22 @@ export default function Game({ username }) {
       window.removeEventListener("keydown", handleKeyDown);
     };
   }, [handleKeyDown]);
+  
 
   const tiles = [];
+  const colorPalette = ["#f87171", "#93c5fd", "green", "#8b5cf6", "#fdba74", "#525252"];
+
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       let tileColor = "white";
       if (i === position.y && j === position.x) {
         tileColor = "yellow";
-      } else if (i === 8 && j === 8) {
+      } else if (i === 9 && j === 9) {
         tileColor = "green";
-      } else if (i % 2 === 0 && j % 2 === 0) {
-        tileColor = "gray";
+      } else {
+        tileColor = colorPalette[Math.floor(Math.random() * colorPalette.length)];
       }
+      
       tiles.push(
         <div
           key={`${i}-${j}`}
@@ -86,103 +91,61 @@ export default function Game({ username }) {
   }
 
   return (
-    <div
-      style={{
-        backgroundColor: "#1d1f25",
-        color: "#fff",
-        fontFamily: "monospace",
-        padding: "20px",
-        borderRadius: "10px",
-        boxShadow: "2px 2px 10px #000",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <h1
-          style={{
-            textAlign: "center",
-            fontSize: "40px",
-            marginBottom: "20px",
-            textShadow: "1px 1px #000",
-          }}
-        >
-          Bienvenue {username}
-        </h1>
-        <p style={{ textAlign: "center", fontSize: "18px", marginBottom: "20px" }}>
-        Utilisez les touches fléchées pour déplacer le pion jusqu'à l'arrivée 'Vous êtes le pion jaune, l'arrivée le pion vert' !
-      </p>
+    <div className='main'>
+      <div className="touch-area">
+      <div className='container'>
+        {!gameOver && (
+          <><h1 className='container-h1'>
+              Bienvenue {username}
+            </h1><p className='container-p'>
+                Utilisez les touches fléchées pour déplacer le pion jusqu'à l'arrivée 'Vous êtes le pion jaune' !
+              </p><p className='container-p'>
+                Une seule case ne change jamais de couleur... c'est l'arrivée !
+              </p></>
+        )}
+        
         {gameOver ? (
-          <div className="game-over" style={{ textAlign: "center", fontSize: "25px", marginBottom: "20px" }}>
-            Maintenant que vous m'avez trouvé vous pouvez m'engager =D !
-            <br />
-            <div>
-            <button
-            onClick={() => window.location.reload(false)}
-            style={{
-              backgroundColor: "#ffdf00",
-              color: "#000",
-              fontFamily: "monospace",
-              padding: "10px 20px",
-              fontSize: "24px",
-              borderRadius: "10px",
-              border: "none",
-              boxShadow: "2px 2px 10px #000",
-              cursor: "pointer",
-              textShadow: "1px 1px #000",
-              transition: "all 0.3s ease",
-              marginTop: "20px",
-              
-              marginRight: "25px"
-            }}
-            onMouseOver={(e) => {
-              e.target.style.backgroundColor = "#000";
-              e.target.style.color = "#ffdf00";
-              e.target.style.textShadow = "none";
-            }}
-            onMouseOut={(e) => {
-              e.target.style.backgroundColor = "#ffdf00";
-              e.target.style.color = "#000";
-              e.target.style.textShadow = "1px 1px #000";
-            }}
-          >
-            Redémarrer
-          </button>
-          <a href="https://engagez-moi.surge.sh" target="_blank"><button
-            style={{
-              backgroundColor: "#ffdf00",
-              color: "#000",
-              fontFamily: "monospace",
-              padding: "10px 20px",
-              fontSize: "24px",
-              borderRadius: "10px",
-              border: "none",
-              boxShadow: "2px 2px 10px #000",
-              cursor: "pointer",
-              textShadow: "1px 1px #000",
-              transition: "all 0.3s ease",
-              marginTop: "20px",
-            }}
-            onMouseOver={(e) => {
-              e.target.style.backgroundColor = "#000";
-              e.target.style.color = "#ffdf00";
-              e.target.style.textShadow = "none";
-            }}
-            onMouseOut={(e) => {
-              e.target.style.backgroundColor = "#ffdf00";
-              e.target.style.color = "#000";
-              e.target.style.textShadow = "1px 1px #000";
-            }}
-          >
-            Me contacter
-          </button></a>
-          </div>
-          </div>
+          <><h1 className="container-h1">
+              Bravo {username}
+            </h1><div className="game-over">
+                Maintenant que vous m'avez trouvé vous pouvez m'engager =D !
+                <br />
+                <div>
+                  <button className='game-over-button'
+                    onClick={() => window.location.reload(false)}
+                    onMouseOver={(e) => {
+                      e.target.style.backgroundColor = "#000";
+                      e.target.style.color = "#ffdf00";
+                      e.target.style.textShadow = "none";
+                    } }
+                    onMouseOut={(e) => {
+                      e.target.style.backgroundColor = "#ffdf00";
+                      e.target.style.color = "#000";
+                      e.target.style.textShadow = "1px 1px #000";
+                    } }
+                  >
+                    Redémarrer
+                  </button>
+                  <a href="https://engagez-moi.surge.sh" target="_blank"><button className="game-over-button"
+                    onMouseOver={(e) => {
+                      e.target.style.backgroundColor = "#000";
+                      e.target.style.color = "#ffdf00";
+                      e.target.style.textShadow = "none";
+                    } }
+                    onMouseOut={(e) => {
+                      e.target.style.backgroundColor = "#ffdf00";
+                      e.target.style.color = "#000";
+                      e.target.style.textShadow = "1px 1px #000";
+                    } }
+                  >
+                    Me contacter
+                  </button></a>
+                </div>
+              </div></>
         ) : (
           <div className="game-board">{tiles}</div>
         )}
+      </div>
       </div>
     </div>
   );
